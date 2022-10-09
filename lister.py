00925e06c1ed22accc5c75bb40efd3049a8b931f
Adrian Sanz <asanz@mrmilu.com>
2022-10-09 18:01:41 +0200

    Make REGIONS required

--- lister.py
+++ lister.py
@@ -82,7 +82,7 @@ def region_lister(profile: str) -> list:
     return REGIONS
 
 
-def get_ec2(profile: str, region: str = None, REGIONS: list = None) -> object:
+def get_ec2(profile: str, region: str = None, REGIONS: list) -> object:
     """
     Return a boto3 ec2 session object.
 
@@ -105,7 +105,7 @@ def get_ec2(profile: str, region: str = None, REGIONS: list = None) -> object:
     return session.resource("ec2")
 
 
-def lister(REGIONS: list = None) -> None:
+def lister(REGIONS: list) -> None:
     """
     List how many instances we have for each region.
 
