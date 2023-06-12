47b643fa72da1de8458f0b2a7771f6e50d04eb63
Adrian Sanz <38634224+adsanz@users.noreply.github.com>
2023-06-12 06:51:32 -0700

    Release fix (#9)
    * Fixed bugs introduced by PR, profile is not mandatory anymore, credentials from ENV will be valid too
    
    * Refactor test
    
    ---------
    
    Co-authored-by: adsanz <adrian.sanz@atalantago.com>

--- lister.py
+++ lister.py
@@ -6,7 +6,7 @@ import signal  # trap Ctrl-c for show_instance cleaner exit
 from typing import Optional
 from threading import Thread
 from rich import box
-
+from os import environ
 import boto3
 from botocore.exceptions import ProfileNotFound
 from rich.json import JSON
@@ -15,6 +15,7 @@ from rich.table import Table
 
 # stubs
 from mypy_boto3_ec2 import EC2ServiceResource
+
 
 ERROR_STYLE = "bold red"
 WARNING_STYLE = "bold yellow"
@@ -53,6 +54,9 @@ def parse_args(args: Optional[list] = None):
     - Find out how many instances per region you have
       lister.py -p leo -l
 
+    - If you don't require a profile, env variables are supported, just omit the -p flag (it requires -r)
+      lister.py -r eu-west-1 -l
+
     WARNING: if no region is defined, a random one will be used.
     """,
         formatter_class=argparse.RawTextHelpFormatter,
@@ -67,7 +71,7 @@ def parse_args(args: Optional[list] = None):
         "-p",
         "--profile",
         help="Profile to authenticate",
-        required=True,
+        required=False,
         type=str,
     )
     parser.add_argument(
@@ -130,7 +134,43 @@ def handler(signum, frame) -> None:
     exit(2)
 
 
-def region_lister(profile: str, options: dict) -> list:
+def credentials_handler(
+    region_name: str, profile: Optional[str] = None
+) -> boto3.session.Session:
+    """
+    This function decides if we use a profile or if we stick to environment credentials.
+    """
+    key_id = environ.get("AWS_ACCESS_KEY_ID")
+    secret_key = environ.get("AWS_SECRET_ACCESS_KEY")
+    if environ.get("AWS_SESSION_TOKEN"):
+        session_token = environ.get("AWS_SESSION_TOKEN")
+    if key_id and secret_key:
+        if session_token:
+            session = boto3.Session(
+                region_name=region_name,
+                aws_access_key_id=key_id,
+                aws_secret_access_key=secret_key,
+                aws_session_token=session_token,
+            )
+        else:
+            session = boto3.Session(
+                region_name=region_name,
+                aws_access_key_id=key_id,
+                aws_secret_access_key=secret_key,
+            )
+    else:
+        try:
+            session = boto3.Session(profile_name=profile, region_name=region_name)
+        except ProfileNotFound as e:
+            console.log(
+                f":warning: Profile {profile} not found. Exiting...", style=ERROR_STYLE
+            )
+            console.error(e)
+            exit(1)
+    return session
+
+
+def region_lister(options: dict, profile: Optional[str] = None) -> list:
     """
     List all regions from AWS instead of hardcoding them.
 
@@ -140,7 +180,7 @@ def region_lister(profile: str, options: dict) -> list:
     Return:
         List of regions.
     """
-    session = boto3.Session(profile_name=profile, region_name="us-east-1")
+    session = credentials_handler("us-east-1", profile)
 
     if options.get("localstack"):
         client = session.client("ec2", endpoint_url="http://localhost:4566")
@@ -151,7 +191,10 @@ def region_lister(profile: str, options: dict) -> list:
 
 
 def get_ec2(
-        profile: str, regions: list, options: dict, region: Optional[str] = None
+    regions: list,
+    options: dict,
+    region: Optional[str] = None,
+    profile: Optional[str] = None,
 ) -> EC2ServiceResource:
     """
     Return a boto3 ec2 session object.
@@ -164,9 +207,9 @@ def get_ec2(
     Return:
         boto3 ec2 session object.
     """
+    session = credentials_handler(region, profile)
     if region is None:
         try:
-            session = boto3.Session(profile_name=profile, region_name=region)
             if options.get("localstack"):
                 return session.resource("ec2", endpoint_url="http://localhost:4566")
             else:
@@ -185,7 +228,6 @@ def get_ec2(
         )
         exit(1)
 
-    session = boto3.Session(profile_name=profile, region_name=region)
     if options.get("localstack"):
         return session.resource("ec2", endpoint_url="http://localhost:4566")
     else:
@@ -203,23 +245,25 @@ class ListerThreading(Thread):
     """
 
     def __init__(
-            self, profile: str, region: str, regions: list, arg_list: dict, *args, **kwargs
+        self, profile: str, region: str, regions: list, arg_list: dict, *args, **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
         self.config = {
             "profile": profile,
             "region": region,
             "regions": regions,
-            "args": arg_list,
+            "options": arg_list,
         }
         for key, value in self.config.items():
             setattr(self, key, value)
 
     def run(self) -> None:
-        if self.args.get("list"):
+        if self.options.get("list"):
             ec2 = get_ec2(**self.config)
             instances = list(ec2.instances.all())
-            color, style = ("red", ERROR_STYLE) if not instances else ("white", "bold green")
+            color, style = (
+                ("red", ERROR_STYLE) if not instances else ("white", "bold green")
+            )
             msg = (
                 f"Found [bold underline {color} on black]{len(instances)}[/] instances on"
                 f"region [bold underline white on black]{self.region}[/]"
@@ -231,7 +275,7 @@ class ListerThreading(Thread):
             """
             pass
 
-    def start(self) -> 'ListerThreading':
+    def start(self) -> "ListerThreading":
         super().start()
         return self
 
@@ -249,8 +293,13 @@ def lister(regions: list, options: dict) -> None:
     threads = []
     with console.status("[bold green]Getting instances... [/]", spinner="dots"):
         threads = [
-            ListerThreading(region=region, profile=str(options.get("profile")), regions=regions, arg_list=options)
-            .start() for region in regions
+            ListerThreading(
+                region=region,
+                profile=str(options.get("profile")),
+                regions=regions,
+                arg_list=options,
+            ).start()
+            for region in regions
         ]
 
         [thread.join(1) for thread in threads if thread.is_alive()]
@@ -276,15 +325,39 @@ def show_instance(ec2: EC2ServiceResource, instance_id: str) -> None:
         )
         table.add_column("Attribute", style="white bold dim", width=30)
         table.add_column("Value", style="white dim")
-        rows = {}
-        row_names = ("ID", "Type", "State", "Launch Time", "IPublic IP", "Private IP", "Public DNS", "Private DNS",
-                     "Key Name", "IAM Role", "VPC ID", "Subnet ID", "Security Groups", "Tags")
+        row_names = (
+            "ID",
+            "Type",
+            "State",
+            "Launch Time",
+            "Public IP",
+            "Private IP",
+            "Public DNS",
+            "Private DNS",
+            "Key Name",
+            "IAM Role",
+            "VPC ID",
+            "Subnet ID",
+            "Security Groups",
+            "Tags",
+        )
 
-        row_values = (instance.id, instance.instance_type, instance.state["Name"], str(instance.launch_time),
-                      instance.public_ip_address, instance.private_ip_address, instance.public_dns_name,
-                      instance.private_dns_name, instance.key_name, JSON(json.dumps(instance.iam_instance_profile)),
-                      instance.vpc_id, instance.subnet_id, JSON(json.dumps(instance.security_groups)),
-                      JSON(json.dumps(instance.tags)))
+        row_values = (
+            instance.id,
+            instance.instance_type,
+            instance.state["Name"],
+            str(instance.launch_time),
+            instance.public_ip_address,
+            instance.private_ip_address,
+            instance.public_dns_name,
+            instance.private_dns_name,
+            instance.key_name,
+            JSON(json.dumps(instance.iam_instance_profile)),
+            instance.vpc_id,
+            instance.subnet_id,
+            JSON(json.dumps(instance.security_groups)),
+            JSON(json.dumps(instance.tags)),
+        )
 
         for name, value in zip(row_names, row_values):
             table.add_row(f"Instance {name}", value)
@@ -304,8 +377,10 @@ def build_filter(args: dict) -> list:
     """
     filter_list = [{"Name": "instance-state-name", "Values": ["running"]}]
     filter_params = (args.get("filter_key", []), args.get("filter_value", []))
-    filter_list.extend([{"Name": fk, "Values": fv.split(",")} for fk, fv in zip(*filter_params) if all(filter_params)])
-
+    if filter_params != (None, None):
+        filter_list.extend(
+            [{"Name": fk, "Values": fv.split(",")} for fk, fv in zip(*filter_params)]
+        )
     return filter_list
 
 
@@ -320,24 +395,30 @@ def main_list(ec2: EC2ServiceResource, args: dict) -> None:
             name = ""
 
             # No need to check if private IPs are empty, since AWS will always assign a private IP to instances
-            priv_ip_list = [priv_ip["PrivateIpAddress"] for priv_ip in instance.network_interfaces_attribute]
+            priv_ip_list = [
+                priv_ip["PrivateIpAddress"]
+                for priv_ip in instance.network_interfaces_attribute
+            ]
 
             instance_tags = instance.tags if instance.tags else list()
             tag_key, tag_value = [tag["Key"] for tag in instance.tags], [
                 tag["Value"] for tag in instance.tags
             ]
 
-            name = ([tags["Value"] for tags in instance_tags if tags["Key"] == "Name"] + [name])[0]
+            name = (
+                [tags["Value"] for tags in instance_tags if tags["Key"] == "Name"]
+                + [name]
+            )[0]
 
-            tag_key, tag_value = args.get("not_show_tags", (tag_key, tag_key))
-
-            if len(tag_key) > 3:
-                console.print(
-                    f"[bold red]Instance {instance.id} has more than 3 tags, only the first 3 will be shown.[/]"
-                )
-                tag_key = tag_key[:3]
-                tag_value = tag_value[:3]
-
+            if args.get("not_show_tags"):
+                tag_key, tag_value = None, None
+            else:
+                if len(tag_key) > 3:
+                    console.print(
+                        f"[bold red]Instance {instance.id} has more than 3 tags, only the first 3 will be shown.[/]"
+                    )
+                    tag_key = tag_key[:3]
+                    tag_value = tag_value[:3]
             ec2_list.append(
                 [
                     instance.instance_id,
@@ -358,7 +439,14 @@ def main_list(ec2: EC2ServiceResource, args: dict) -> None:
         )
 
         params = dict(justify="left", style="cyan", no_wrap=True)
-        headers = ["Instance ID", "Name", "Public IP", "Private IP", "Uptime (days)", "Tags", ]
+        headers = [
+            "Instance ID",
+            "Name",
+            "Public IP",
+            "Private IP",
+            "Uptime (days)",
+            "Tags",
+        ]
 
         [ec2_table.add_column(header, **params) for header in headers]
         [ec2_table.add_row(*row) for row in ec2_list]
@@ -376,11 +464,16 @@ def main():
 
         install(show_locals=True)
 
-    profile_name = opts.get("profile")
+    if opts.get("profile"):
+        profile_name = opts.get("profile")
     region_name = opts.get("region")
 
     try:
-        regions = region_lister(profile=profile_name, options=opts)
+        regions = (
+            region_lister(profile=profile_name, options=opts)
+            if opts.get("profile")
+            else region_lister(options=opts)
+        )
     except ProfileNotFound:
         console.log(
             f":warning: Profile '{profile_name}' is not valid. Exiting...",
@@ -388,8 +481,10 @@ def main():
         )
         raise
 
-    ec2 = get_ec2(
-        profile=profile_name, regions=regions, region=region_name, options=opts
+    ec2 = (
+        get_ec2(profile=profile_name, regions=regions, region=region_name, options=opts)
+        if opts.get("profile")
+        else get_ec2(regions=regions, region=region_name, options=opts)
     )
 
     if opts.get("list"):
--- tests.py
+++ tests.py
@@ -1,6 +1,78 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import copy
+from unittest import TestCase
+from unittest.mock import patch, Mock
 
-def test_minimal():
-    assert True
+from faker import Faker
+import lister
+
+faker = Faker()
+
+
+class TestListerThreading(TestCase):
+    def get_instace(self, **kwargs):
+        config = {
+            "profile": faker.word(),
+            "region": faker.word(),
+            "regions": [faker.word() for _ in range(4)],
+            "arg_list": {faker.word(): faker.word() for _ in range(3)},
+        }
+        config.update(kwargs)
+        lister_threading = lister.ListerThreading(**config)
+
+        return config, lister_threading
+
+    def test_init(self):
+        kwargs, lister_threading = self.get_instace()
+        expected_config = copy.deepcopy(kwargs)
+        expected_config.update({"options": kwargs["arg_list"]})
+        del expected_config["arg_list"]
+
+        assert lister_threading.config == expected_config
+
+    def test_run_ok(self):
+        params = {"arg_list": {"list": faker.word()}}
+        kwargs, lister_threading = self.get_instace(**params)
+        kwargs["options"] = params["arg_list"]
+        del kwargs["arg_list"]
+
+        all_mocked = (faker.word() for _ in range(4))
+        instances_mock = Mock()
+        instances_mock.all = Mock(return_value=all_mocked)
+        get_ec2_mocked = Mock()
+        get_ec2_mocked.instances = instances_mock
+        console_mocked = Mock()
+        console_mocked.log = Mock()
+        expected_msg = (
+            f"Found [bold underline white on black]4[/] instances on"
+            f"region [bold underline white on black]{kwargs['region']}[/]"
+        )
+        with patch.object(lister, "get_ec2", return_value=get_ec2_mocked) as e1:
+            with patch.object(lister, "console", console_mocked) as e2:
+                lister_threading.run()
+
+                e1.assert_called_with(**kwargs)
+                e2.log.assert_called_with(expected_msg, style="bold green")
+
+    def test_run_fail(self):
+        kwargs, lister_threading = self.get_instace()
+        get_ec2_mocked = Mock()
+        console_mocked = Mock()
+        console_mocked.log = Mock()
+
+        with patch.object(lister, "get_ec2", return_value=get_ec2_mocked) as e1:
+            with patch.object(lister, "console", console_mocked) as e2:
+                lister_threading.run()
+
+                e1.assert_not_called()
+                e2.log.assert_not_called()
+
+    def test_start(self):
+        start_mocked = Mock()
+        with patch.object(lister.Thread, "start", start_mocked) as e1:
+            kwargs, lister_threading = self.get_instace()
+            lister_threading.start()
+
+            e1.assert_called()
